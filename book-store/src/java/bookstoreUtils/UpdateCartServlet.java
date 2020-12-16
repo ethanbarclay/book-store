c706dd416c9a5bfbdc8b206f6bfd3df30bed1b22
microsocks <barclayethan@gmail.com>
2020-12-15 21:00:22 -0500

    added servlet functions

--- book-store/src/java/bookstoreUtils/UpdateCartServlet.java
+++ book-store/src/java/bookstoreUtils/UpdateCartServlet.java
@@ -7,106 +7,106 @@ package bookstoreUtils;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author tduffy
  */
-//public class UpdateCartServlet extends HttpServlet {
-//
-//    /**
-//     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-//     * methods.
-//     *
-//     * @param request servlet request
-//     * @param response servlet response
-//     * @throws ServletException if a servlet-specific error occurs
-//     * @throws IOException if an I/O error occurs
-//     */
-//    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        response.setContentType("text/html;charset=UTF-8");
-//        try (PrintWriter out = response.getWriter()) {
-//            /* TODO output your page here. You may use following sample code. */
-//            out.println("<!DOCTYPE html>");
-//            out.println("<html>");
-//            out.println("<head>");
-//            out.println("<title>Servlet UpdateCartServlet</title>");            
-//            out.println("</head>");
-//            out.println("<body>");
-//            out.println("<h1>Servlet UpdateCartServlet at " + request.getContextPath() + "</h1>");
-//            out.println("</body>");
-//            out.println("</html>");
-//        }
-//    }
-//
-//    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-//    /**
-//     * Handles the HTTP <code>GET</code> method.
-//     *
-//     * @param request servlet request
-//     * @param response servlet response
-//     * @throws ServletException if a servlet-specific error occurs
-//     * @throws IOException if an I/O error occurs
-//     */
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        //processRequest(request, response);
-//    }
-//
-//    /**
-//     * Handles the HTTP <code>POST</code> method.
-//     *
-//     * @param request servlet request
-//     * @param response servlet response
-//     * @throws ServletException if a servlet-specific error occurs
-//     * @throws IOException if an I/O error occurs
-//     */
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        //processRequest(request, response);
-//        HttpSession session = request.getSession(true);
-//        ShoppingCart cart = (ShoppingCart)session.getAttribute("cart");
-//        if(cart==null){
-//            response.sendRedirect("showCart.jsp");
-//        }else{
-//            //We have the cart, let's update it
-//            int itemCount = cart.getItemCount();
-//            int quant = 0;
-//            for(int i=itemCount-1; i>=0; i--){
-//                quant = Integer.parseInt(request.getParameter("item" + i));
-//                //Get the Item
-//                Item item = cart.getItem(i);
-//                if(quant == 0){
-//                    //Remove the item form the cart
-//                    cart.removeItem(item);
-//                }else{
-//                    //Set its quantity
-//                    item.setQuantity(quant);
-//                }
-//            }
-//            //Put the cart back in the session
-//            session.setAttribute("cart",cart);
-//            //go look at it!
-//            response.sendRedirect("showCart.jsp");
-//        }
-//    }
-//
-//    /**
-//     * Returns a short description of the servlet.
-//     *
-//     * @return a String containing servlet description
-//     */
-//    @Override
-//    public String getServletInfo() {
-//        return "Servlet for updating the Shopping Cart";
-//    }// </editor-fold>
-//
-//}
+public class UpdateCartServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet UpdateCartServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet UpdateCartServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        HttpSession session = request.getSession(true);
+        ShoppingCart cart = (ShoppingCart)session.getAttribute("cart");
+        if(cart==null){
+            response.sendRedirect("showCart.jsp");
+        }else{
+            //We have the cart, let's update it
+            int itemCount = cart.getItemCount();
+            int quant = 0;
+            for(int i=itemCount-1; i>=0; i--){
+                quant = Integer.parseInt(request.getParameter("item" + i));
+                //Get the Item
+                Item item = cart.getItem(i);
+                if(quant == 0){
+                    //Remove the item form the cart
+                    cart.removeItem(item);
+                }else{
+                    //Set its quantity
+                    item.setQuantity(quant);
+                }
+            }
+            //Put the cart back in the session
+            session.setAttribute("cart",cart);
+            //go look at it!
+            response.sendRedirect("showCart.jsp");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Servlet for updating the Shopping Cart";
+    }// </editor-fold>
+
+}
